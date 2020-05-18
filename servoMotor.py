650651c19fdc0746dcbcd8d60bf1596e517db343
pmasa <masamuna.pedro@hotmail.com>
2020-05-19 01:26:30 +0200

    pir sensor test ok

--- servoMotor.py
+++ servoMotor.py
@@ -1,14 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
+import threading 
+
+
 GPIO.setmode(GPIO.BCM)
 
-relay_pin = 17 
+relay_pin = 27
 servo1_pin = 6
 servo2_pin = 5
-PIR_SENSOR = 23
+PIR_SENSOR = 17
 
-GPIO.setup(PIR_SENSOR, GPIO.IN)
+GPIO.setup(PIR_SENSOR, GPIO.IN, GPIO.PUD_DOWN)
 GPIO.setup(servo1_pin, GPIO.OUT)
 GPIO.setup(servo2_pin, GPIO.OUT)
 GPIO.setup(relay_pin, GPIO.OUT)
@@ -29,6 +32,54 @@ MIN_DUTY = 2
 MAX_DUTY = 10
 CENTRE = MIN_DUTY + (MAX_DUTY - MIN_DUTY) / 2
 
+stop = False
+motion = False
+
+i = GPIO.input(PIR_SENSOR)
+time.sleep(5) # wait 5 seconds for PIR to reset.
+
+class Monthread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.encore = True # variable pour arreter le thread sur demande
+        self.enpause = False # variable pour mettre le thread en pause
+
+        self.c = 0 # simple compteur pour affichage
+	self.previous_state = 0
+	self.current_state = 0
+
+    def run(self):
+        global motion
+        while self.encore:
+
+            while self.enpause:
+                time.sleep(15)
+
+            # activite du thread
+            # ...
+	    self.current_state  = GPIO.input(PIR_SENSOR)
+	    time.sleep(4) # wait 4 seconds for PIR to reset.
+	    if self.previous_state == 0  and self.current_state == 1:
+		motion = True
+	    else:
+		print("no motion")
+	    self.previous_state  = self.current_state
+            #self.c += 1
+            #print self.c
+            # ...
+
+    def pause(self):
+        self.enpause = True
+	print("thread en pause...")
+
+    def reprise(self):
+        self.enpause = False
+	print("reprise thread...")
+
+    def stop(self):
+        self.encore = False
+
+
 # To avoid motor trembling
 def no_stress():
     servo1.ChangeDutyCycle(0)
@@ -39,13 +90,18 @@ servo2.start(MIN_DUTY)
 time.sleep(1)
 no_stress()
 
+app=Monthread()
+app.start()
+
 try:
     while True:
-        time.sleep(4)    
-        i = GPIO.input(PIR_SENSOR)
+        time.sleep(1)    
+        #i = GPIO.input(PIR_SENSOR)
         k = 0 
-        if i == 1:
-            while k < 2:
+        #if i == 1 and busy == False:
+	if app.isAlive() and motion:
+	    app.pause()
+            while k < 1:
                 print ("Turning to 90 degrees")
                 servo1.ChangeDutyCycle(MIN_DUTY)
                 servo2.ChangeDutyCycle(MAX_DUTY)
@@ -60,7 +116,7 @@ try:
                 time.sleep(1)
             
 	    print("Rubbing hands ...")
-            time.sleep(1)
+            time.sleep(5)
 
 	    print("Open pump")
             GPIO.output(relay_pin, GPIO.HIGH)
@@ -68,15 +124,22 @@ try:
 
 	    print("Pump stopped")
             GPIO.output(relay_pin, GPIO.LOW)
-        elif i==0:
-            no_stress()
-            k = 0
-            print("No motion")
+	    motion = False
+	    time.sleep(5)
+	    app.reprise()
+	#if z > 20:
+	#    z = 0
+        #elif i==0:
+	#    busy = False
+        #    no_stress()
+        #    k = 0
+        #    print("No motion")
 
 except KeyboardInterrupt:
     print("CTRL-C: Terminating program.")
 finally:
     print("Cleaning up GPIO...")
+    app.stop() 
     servo1.stop()
     servo2.stop()
     GPIO.output(relay_pin, GPIO.LOW)
